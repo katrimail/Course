f485dc0fc4720accc5c10f9d2c48b3a2a6c32c3d
Katrin Mailyan <katin.mailyan31@gmail.com>
2020-12-18 17:00:41 +0300

    Initial Commit

---
+++ Courses/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Courses
+//
+//  Created by Катрин Маилян on 18.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Courses/CoursesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoursesApp.swift
+//  Courses
+//
+//  Created by Катрин Маилян on 18.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct CoursesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
